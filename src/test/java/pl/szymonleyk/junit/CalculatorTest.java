04c94480ed4a07c6216f904070bf446963609b99
Szymon Leyk <leyas@wp.pl>
2020-06-15 12:04:48 +0200

    @DisplayName annotation

--- src/test/java/pl/szymonleyk/junit/CalculatorTest.java
+++ src/test/java/pl/szymonleyk/junit/CalculatorTest.java
@@ -4,42 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
 	@Test
+	@DisplayName("3 + 2 = 5")
 	void addTest1() {
-		assertEquals(5, Calculator.add(3,2));
+		assertEquals(5, Calculator.add(3, 2));
 	}
-	
+
 	@Test
+	@DisplayName("3 + 2 + 5 + 6 = 16")
 	void addTest2() {
-		assertEquals(16, Calculator.add(3,2,5,6));
+		assertEquals(16, Calculator.add(3, 2, 5, 6));
 	}
-	
+
 	@Test
+	@DisplayName("55 + 2 != 5")
 	void addTest3() {
-		assertNotEquals(5, Calculator.add(55,2));
+		assertNotEquals(5, Calculator.add(55, 2));
 	}
-	
+
 	@Test
+	@DisplayName("4 + 4 > 3")
 	void addTest4() {
-		assertTrue(Calculator.add(4,4) > 3);
+		assertTrue(Calculator.add(4, 4) > 3);
 	}
-	
+
 	@Test
+	@DisplayName("100 - 45 = 55")
 	void substractTest() {
 		assertEquals(55, Calculator.substract(100, 45));
 	}
-	
+
 	@Test
+	@DisplayName("2 / 2 = 1.0")
 	void divideTest() {
-		assertEquals(1.0, Calculator.divide(2,2));
+		assertEquals(1.0, Calculator.divide(2, 2));
 	}
-	
+
 	@Test
+	@DisplayName("5 * 5 = 25")
 	void multipleTest() {
-		assertEquals(25, Calculator.multiple(5,5));
+		assertEquals(25, Calculator.multiple(5, 5));
 	}
 }
